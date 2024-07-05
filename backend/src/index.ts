bcaadabea921713e4d6155c7a04eaa0afcb4e0ff
Gautam Gulati <101964337+GautamGulati28@users.noreply.github.com>
2024-07-05 13:48:07 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,7 @@ import { outcomes } from "./outcomes";
 import cors from "cors";
 
 const app = express();
-app.use(cors({
-    origin: *,
-    credentials: true,
-}))
+app.use(cors())
 
 const TOTAL_DROPS = 16;
 
@@ -55,8 +52,5 @@ app.post("/game", (req, res) => {
 });
 
 
-app.post("/test",(req,res) => {
-    res.send("hello world')
-});
 
 app.listen(3000)

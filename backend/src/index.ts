9ff33e747f984434c0d3303c650ba5c5b108956d
Gautam Gulati <101964337+GautamGulati28@users.noreply.github.com>
2024-07-05 13:34:10 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,6 +39,7 @@ app.post("/game", (req, res) => {
             pattern.push("L")
         }
     }
+    
 
     const multiplier = MULTIPLIERS[outcome];
     const possiblieOutcomes = outcomes[outcome];
@@ -48,6 +49,11 @@ app.post("/game", (req, res) => {
         multiplier,
         pattern
     });
+});
+
+
+app.post("/test",(req,res) => {
+    res.send("hello world')
 });
 
 app.listen(3000)
